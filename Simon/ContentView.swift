b08128d66d0f83fac29b18cbfeac3164e9bf08e6
Caden Christesen <cadenchristesen@gmail.com>
2023-11-30 14:17:36 -0600

    more doen

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var colorDisplay = [ColorDisplay(color: .green), ColorDisplay(color: .red), ColorDisplay(color: .yellow), ColorDisplay(color: .blue)]
+    @State private var timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+    
     @State private var flash = [false, false, false, false]
     var body: some View {
         VStack {
@@ -17,20 +19,35 @@ struct ContentView: View {
             HStack{
                 colorDisplay[0]
                     .opacity(flash[0] ? 1 : 0.4)
+                    .onTapGesture {
+                        flashColorDisplay(index: 0)}
                 colorDisplay[1]
                     .opacity(flash[1] ? 1 : 0.4)
+                    .onTapGesture {
+                        flashColorDisplay(index: 1)}
             }
             HStack{
                 colorDisplay[2]
                     .opacity(flash[2] ? 1 : 0.4)
+                    .onTapGesture {
+                        flashColorDisplay(index: 2)}
                 colorDisplay[3]
                     .opacity(flash[3] ? 1 : 0.4)
+                    .onTapGesture {
+                        flashColorDisplay(index: 3)}
             }
             
         }
         .preferredColorScheme(.dark)
-        .padding()
     }
+    
+    func flashColorDisplay(index: Int) {
+        flash[index].toggle()
+        withAnimation(.easeInOut(duration: 0.5)) {
+            flash[index].toggle()
+        }
+    }
+    
 }
 struct ColorDisplay: View {
     let color: Color
@@ -39,6 +56,7 @@ struct ColorDisplay: View {
             .fill(color)
             .frame(width: 100, height: 100, alignment: .center)
             .padding()
+        
     }
 }
 
