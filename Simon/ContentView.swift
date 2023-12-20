d8c2b5efb1e53dc2b74bb5922f1319b8f7dca552
Caden Christesen <cadenchristesen@gmail.com>
2023-12-20 09:38:44 -0600

    cureent build fails to keep the same sequence between user inputs and fails to increase in size, however now it will automatically go again after user input

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -1,3 +1,10 @@
+//
+//  ContentView.swift
+//  Simon
+//
+//  Created by Caden Christesen on 11/13/23.
+//
+
 import SwiftUI
 
 struct ContentView: View {
@@ -7,37 +14,43 @@ struct ContentView: View {
     @State private var index: Int = 0
     @State private var timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
     @State private var isPlayingSequence = false
-
+    @State private var userInput: [Int] = []
     var body: some View {
         VStack {
             Text("Simon")
-                .font(.system(size: 72))
+                .font(Font.custom("Verdana Bold", size: 36))
             Button("Start") {
                 startGame()
             }
+            .font(Font.custom("Verdana Bold", size: 24))
             .disabled(isPlayingSequence)
             HStack {
                 colorDisplay[0]
                     .opacity(flash[0] ? 1 : 0.4)
                     .onTapGesture {
                         flashColorDisplay(index: 0)
+                        processUserInput(index: 0)
                     }
                 colorDisplay[1]
                     .opacity(flash[1] ? 1 : 0.4)
                     .onTapGesture {
                         flashColorDisplay(index: 1)
+                        processUserInput(index: 1)
                     }
             }
+            
             HStack {
                 colorDisplay[2]
                     .opacity(flash[2] ? 1 : 0.4)
                     .onTapGesture {
                         flashColorDisplay(index: 2)
+                        processUserInput(index: 2)
                     }
                 colorDisplay[3]
                     .opacity(flash[3] ? 1 : 0.4)
                     .onTapGesture {
                         flashColorDisplay(index: 3)
+                        processUserInput(index: 3)
                     }
             }
             .preferredColorScheme(.dark)
@@ -51,18 +64,30 @@ struct ContentView: View {
                     index = 0
                     sequence.append(Int.random(in: 0...3))
                     isPlayingSequence = false
+                    userInput = []
                 }
             }
         }
     }
-
+    func processUserInput(index: Int) {
+        userInput.append(index)
+        if userInput == sequence {
+            if userInput.count == sequence.count {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    startGame()
+                }
+            }
+        } else {
+            print("Game Over")
+            startGame()
+        }
+    }
     func flashColorDisplay(index: Int) {
         flash[index].toggle()
         withAnimation(.easeInOut(duration: 0.5)) {
             flash[index].toggle()
         }
     }
-
     func startGame() {
         index = 0
         sequence = [Int.random(in: 0...3)]
